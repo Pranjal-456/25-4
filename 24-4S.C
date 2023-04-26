7448a4f3f6be8b0e970f60bd3d7331a2e1d4b6e2
Pranjal-456 <131869517+Pranjal-456@users.noreply.github.com>
2023-04-26 16:00:02 +0530

    Add files via upload

---
+++ 24-4S.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+
+int a,b;
+a=50;
+b=10;
+a=a+b;
+b=a-b;
+a=a-b;
+clrscr();
+printf("value of a=%d",a);
+printf("\nvalue of b=%d",b);
+getch();
+
+ }
